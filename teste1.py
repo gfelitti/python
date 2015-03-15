01a4c9c6815d789c0fb06a41371a0208adfe9bb1
guilherme felitti <gfelitti@gmail.com>
2015-03-15 17:53:02 -0300

    simplegui - lesson1

--- teste1.py
+++ teste1.py
@@ -1,1 +1,9 @@
-print "hello world!"
+import simpleguitk as simplegui
+def tick():
+    print "tick!"
+
+# Register handler
+timer = simplegui.create_timer(1000, tick)
+
+# Start timer
+timer.start()
