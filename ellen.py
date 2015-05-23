70a17811cfa37ddb37d2ce3425f450c415ef3e13
guilherme felitti <gfelitti@gmail.com>
2015-05-23 15:38:46 -0300

    o primeiro aplicativo fora das aulas

---
+++ ellen.py
@@ -0,0 +1,15 @@
+def falaellen(fala):
+    if fala == "Gigi":
+        print ("Oi")
+    elif fala == "Cê me ama?":
+        print ("Amo")
+    elif fala == "Ama muito?":
+        print ("Muito")
+    else:
+        print ("Coisinha linda, fala de novo")
+    return
+
+falaellen("Gigi")
+falaellen("Cê me ama?")
+falaellen("Coisinha linda")
+falaellen("Ama muito?")
